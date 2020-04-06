bdc556ec61cc7d9ba3e20d240b6e526e595a1a0a
ArcaXbcXde <marcuscuz69@hotmail.com>
2020-04-06 12:58:45 -0300

    algumas mudanças

--- PI 2020/Assets/Project/Scripts/Buttons.cs
+++ PI 2020/Assets/Project/Scripts/Buttons.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour {
+
+    //Ao ser chamado vai pra o menu inicial
+    public void BtMenu() {
+
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    //Ao ser chamado vai pra a DevRoom (alterar depois)
     public void BtContinue () {
         
-        SceneManager.LoadScene("DevRoom"); //Ao ser chamado vai pra a DevRoom (alterar depois)
+        SceneManager.LoadScene("DevRoom");
     }
 
-    public void BtMenu () {
+    //Ao ser chamado vai pra o menu de opções
+    public void BtOptions() {
 
-        SceneManager.LoadScene("StartMenu"); //Ao ser chamado vai pra o Menu inicial
+        SceneManager.LoadScene("Options");
     }
 
+    //Ao ser chamado fecha a aplicação
     public void BtQuit () {
         
-        Application.Quit();               //Ao ser chamado fecha a aplicação
+        Application.Quit();
     }
-    
 }
